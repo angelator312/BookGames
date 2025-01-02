3ff2f03c7704641cb81e237e1f119abce04b9aa6
angelator312 <angel.raev2012@gmail.com>
2025-01-02 16:12:09 +0200

    Преди голяма промяна

--- app/routes/myBook.see.$book.$glava.tsx
+++ app/routes/myBook.see.$book.$glava.tsx
@@ -1,13 +1,12 @@
 import type { Text as Text2 } from "~/utils/textStore";
 import { redirect } from "@remix-run/node";
 import type { ActionFunctionArgs, LoaderFunctionArgs } from "@remix-run/node";
-import { useLoaderData, useNavigate, useSearchParams } from "@remix-run/react";
+import { useLoaderData, useSearchParams } from "@remix-run/react";
 import getTextStore from "~/utils/textStore";
 import { requireUserId } from "~/utils/session.server";
 import DropDown1 from "~/components/dropdown";
 import { useEffect, useState } from "react";
-import { Col, Container, Row, Tab, Tabs } from "react-bootstrap";
-import EditText from "~/components/editText";
+import { Col, Row } from "react-bootstrap";
 import NavYesOrNo from "~/components/navbarYes";
 import Book from "~/components/book";
 import FormComponent from "~/components/formComp";
@@ -48,13 +47,7 @@ export async function loader({ params, request }: LoaderFunctionArgs) {
 }
 export default function Book1() {
   const [searchParams] = useSearchParams();
-  const navigate = useNavigate();
   const [bUrl, gl, t, doN, comments] = useLoaderData<loaderData>();
-  let comm = comments;
-  function update() {
-    textLines = text.split("\n\n");
-    textLines = textLines.slice(2);
-  }
   const [text, setText] = useState(
     //@ts-ignore
     typeof t === "string" ? "проба" : t?.text ?? "проба"
@@ -113,7 +106,7 @@ export default function Book1() {
         <Col sm="1"></Col>
         <Col sm="6">
           <DropDown1
-            url={`/myBook/${bUrl}`}
+            url={`/myBook/see/${bUrl}`}
             // @ts-ignore
             doN={parseInt(doN ?? "15")}
             // @ts-ignore
@@ -122,87 +115,37 @@ export default function Book1() {
         </Col>
       </Row>
       <br />
-      <Tabs
-        defaultActiveKey="preview"
-        id="uncontrolled-tab-example"
-        className="mb-3"
-        key={"space"}
-        
-      >
-
-        {/* <Button eventKey="preview" title="Преглед" onClick={()=>{window.open(`/myBook/see/${bUrl}/${gl}`);}}> */}
-          
-        {/* </Button> */}
-        <Tab eventKey="editAndPreview" title="Редактирай">
-          <Container>
-            <Row>
-              <Col>
-                <EditText
-                  text={text}
-                  text2={text2}
-                  glava={gl ?? "1"}
-                  bUrl={`${bUrl}`}
-                  setText={setText}
-                  setText2={setText2}
-                  priIzvikvane={update}
-                  key={gl}
-                />
-              </Col>
-              <Col>
-                <Book
-                  url={`/myBook/${bUrl}`}
-                  title={"Книга " + bUrl}
-                  almP={`/myBook/${bUrl}/`}
-                  flag={3}
-                  params={{
-                    text,
-                    glava: gl,
-                    text2,
-                  }}
-                  kr={false}
-                />
-              </Col>
-            </Row>
-            <Row>
-              <Col sm="2"></Col>
-              <Col sm="6">
-                <FormComponent
-                  to="/"
-                  textForSubmit="Към главната страница"
-                  method="get"
-                  submitVariant="secondary"
-                />
-              </Col>
-              <Col>
-                <FormComponent
-                  textsHidden={[
-                    text.replace(/\\r/gm, "") ?? "a",
-                    text2.replace(/\\r/gm, "") ?? "a",
-                  ]}
-                  to={`/myBook/${bUrl}/${gl}/save`}
-                  textForSubmit="Запази промените"
-                  submitVariant="danger"
-                />
-              </Col>
-            </Row>
-          </Container>
-        </Tab>
-      </Tabs>
-      <Container>
-        {/* @ts-ignore */}
-        {comm.map((e, i) => (
-          <Row key={e[0]}>
-            <Col sm="7">
-              <NavYesOrNo
-                text={e[0].length > 0 ? `${e[0]}\n от ${e[1]}` : ""}
-                f={(a: any) => {
-                  navigate(`/myBook/${bUrl}/${gl}/deleteComment/${i}?p="sx"`);
-                }}
-              />
-            </Col>
-          </Row>
-        ))}
-      </Container>
+      {/* <Text
+            furst2Lines={furst2Lines}
+            glava={gl ?? "1"}
+            url={`/myBook/${bUrl}`}
+            textLines={textLines}
+            text2Lines={text2}
+            flag1={false}
+            /> */}
+      <Book
+        url={`/myBook/see/${bUrl}`}
+        title={"Книга " + bUrl}
+        almP={`/myBook/see/${bUrl}/`}
+        flag={2}
+        params={{
+          text,
+          glava: gl,
+          text2,
+        }}
+        kr={false}
+      />
+      <Row>
+        <Col sm="2"></Col>
+        <Col sm="6">
+          <FormComponent
+            to="/"
+            textForSubmit="Към главната страница"
+            method="get"
+            submitVariant="secondary"
+          />
+        </Col>
+      </Row>
     </div>
   );
 }
