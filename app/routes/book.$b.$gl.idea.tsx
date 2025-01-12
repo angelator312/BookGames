feb9b93a9cfdd87288a8235e04471ff90d50cf9a
angelator312 <angel.raev2012@gmail.com>
2025-01-12 07:45:59 +0200

    fontSizeFix

--- app/routes/book.$b.$gl.idea.tsx
+++ app/routes/book.$b.$gl.idea.tsx
@@ -7,6 +7,7 @@ import getUserStore from "~/utils/userStore";
 import getTextStore from "~/utils/textStore";
 import { createGorB, requireUserId } from "~/utils/session.server";
 import { Row } from "react-bootstrap";
+import { getDefaultSettings } from "~/utils/User";
 export async function action({ params, request }: ActionFunctionArgs) {
   const form = await request.formData();
   let gl = params.gl;
@@ -33,8 +34,7 @@ export async function loader({ params, request }: LoaderFunctionArgs) {
       let text = await tStore.getText(`${b.text}-${glava}`);
       let myComments=await tStore.getMyComments(params.b??"",glava,a)??[];
       if (!text || !glava) return redirect("/");
-      
-
+      if(!user) return redirect("/login?redirectTo="+request.url);
       let segG = text.text;
       let spec = text.text2;
       createGorB("glava", glava, request);
@@ -48,13 +48,13 @@ export async function loader({ params, request }: LoaderFunctionArgs) {
 export default function Idea() {
   const book = useLoaderData<typeof loader>();
   //console.log(book.glava);
-
+  const settings=book.user.settings??getDefaultSettings();
   //   style={{ padding: 15.4 }}
   //   console.log(book);
   const zagl = book.b.id;
   return (
     <div>
-      <Row>
+      <Row style={{ fontSize: (settings.fontSize ?? 10) / 10 + "rem" }}>
         <Book
           url={`/book/${book.b.text}`}
           title={zagl ?? ""}
@@ -64,7 +64,10 @@ export default function Idea() {
       </Row>
       <Row>
         <div className="space-y-2 sm:text-left bg-i">
-          <IdeaComp url={`/book/${book.b.text}/${book.glava}/idea`}  comments={book.myComments}/>
+          <IdeaComp
+            url={`/book/${book.b.text}/${book.glava}/idea`}
+            comments={book.myComments}
+          />
         </div>
       </Row>
     </div>
