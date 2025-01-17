e9a0a391907a30155b75cdabb479aad258adfc1f
angelator312 <angel.raev2012@gmail.com>
2025-01-17 19:44:59 +0200

    профил pre-release version

---
+++ app/routes/profil.tsx
@@ -0,0 +1,87 @@
+import { Editor } from "@monaco-editor/react";
+import type { LoaderFunctionArgs } from "@remix-run/node";
+import { redirect } from "@remix-run/node";
+import { useLoaderData } from "@remix-run/react";
+import { useState } from "react";
+import { Col, Container, Form, Row, Tab, Tabs } from "react-bootstrap";
+import MenuForHome from "~/components/home.menu";
+import { requireUserId } from "~/utils/session.server";
+import type { SettingsInterface, User, UserData } from "~/utils/User";
+import getUserStore from "~/utils/userStore";
+
+export const loader = async ({ request }: LoaderFunctionArgs) => {
+  const a = await requireUserId(request, false);
+  // console.log(a);
+  // console.log(await knigi(a));
+
+  if (typeof a === "string") {
+    const uStore = await getUserStore();
+    const user = await uStore.getUser(a);
+    if (user) {
+      return [
+        user,
+        user.settings ?? uStore.getDefaultSettings(),
+        user.data ?? uStore.getDefaultUserData(),
+      ];
+    }
+  }
+  return redirect("/");
+};
+type loaderType = [User, SettingsInterface, UserData];
+export default function Settings() {
+  const [user, settings, data] = useLoaderData<loaderType>();
+  const [text, setText] = useState(data.forMe);
+  function handleEditorChange(value: any, event: any) {
+    setText(value);
+  }
+  return (
+    <Container>
+      <MenuForHome
+        //@ts-ignore
+        user={user}
+        settings={settings}
+      />
+      <Tabs>
+        <Tab title="За мен" eventKey={"За мен"}>
+          <Row>
+            <Col>
+              <Form action="/zapaziForMe">
+                <Row>
+                  <Col>
+                    <Form.Label column sm={10}>
+                      Резюме
+                    </Form.Label>
+                  </Col>
+                </Row>
+                <Row>
+                  <Col>
+                    <Editor
+                      options={{
+                        unicodeHighlight: {
+                          ambiguousCharacters: false,
+                        },
+                      }}
+                      height="20vh"
+                      defaultLanguage="bg"
+                      onChange={handleEditorChange}
+                      // name="text"
+                      // placeholder="Здравей,Човече"
+                      defaultValue={text == "" ? "Здравей,Човече" : text}
+                    />
+                  </Col>
+                </Row>
+                <Row>
+                  <Col>
+                    <Form.Control type="submit" value={"Запази"} />
+                    <Form.Control type="hidden" value={text} name="forMe" />
+                    <Form.Control type="hidden" value="/Profil" name="toUrl" />
+                  </Col>
+                </Row>
+              </Form>
+            </Col>
+          </Row>
+        </Tab>
+      </Tabs>
+    </Container>
+  );
+}
---
+++ app/routes/zapaziForMe.tsx
@@ -0,0 +1,24 @@
+import type { LoaderFunctionArgs } from "@remix-run/node";
+import { redirect } from "@remix-run/node";
+import { requireUserId } from "~/utils/session.server";
+import getUserStore from "~/utils/userStore";
+
+export async function loader({ request }: LoaderFunctionArgs) {
+  const form =new URL(request.url).searchParams;
+  if (!form) return redirect("/");
+  //console.log(form);
+  
+  const a = await requireUserId(request, false);
+  if (!a) return redirect("/login");
+  const uStore = await getUserStore();
+  const forMeDescription = form.get("forMe")?.toString();
+  const user = await uStore.getUser(a);
+  if(!user) return redirect("/login");
+  const data=user.data;
+  if (typeof forMeDescription == "string")
+      data.forMe = forMeDescription;
+    
+  //settings.language="bg";
+  uStore.adjustUserData(data, a);
+  return redirect(form.get("toUrl")??"/?successCode=1");
+}
