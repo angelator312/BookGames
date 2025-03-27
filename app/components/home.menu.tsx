333b3a7b40edf1a747bcd496596ac632ffcae842
angelator312 <angelator312@gmail.com>
2025-03-27 05:23:11 +0200

    video things

--- app/components/home.menu.tsx
+++ app/components/home.menu.tsx
@@ -153,7 +153,7 @@ function MenuForHome({
           </Row>
           <Row>
             <Col className="mt-2">
-              <a color="red" href="/videos/NOIT_project102.mp4" className="">
+              <a color="red" href="https://youtu.be/sc0qmDgGpT4" className="">
                 <span className="text-slate-500 font-medium logo text-primary">
                   Примерно видео
                 </span>
